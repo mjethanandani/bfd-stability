70aa26193213532a9a73a85d847156260cb83dc6
Mahesh Jethanandani <mjethanandani@gmail.com>
2021-02-24 20:28:06 -0800

    v08

---
+++ src/download-dependent-models.py
@@ -0,0 +1,42 @@
+import os
+
+list_of_ietf_models =\
+[ ["ietf-crypto-types", "draft-ietf-netconf-crypto-types", "18"],
+  ["ietf-truststore", "draft-ietf-netconf-trust-anchors", "13"],
+  ["ietf-keystore", "draft-ietf-netconf-keystore", "20"],
+  ["ietf-tcp-common", "draft-ietf-netconf-tcp-client-server", "08"],
+  ["ietf-tcp-client", "draft-ietf-netconf-tcp-client-server", "08"],
+  ["ietf-tls-client", "draft-ietf-netconf-tls-client-server", "22"],
+  ["ietf-tls-common", "draft-ietf-netconf-tls-client-server", "22"],
+  ["ietf-tls-server", "draft-ietf-netconf-tls-client-server", "22"],
+  ["ietf-http-client", "draft-ietf-netconf-http-client-server", "05"] ]
+
+
+def fetch_and_extract(draft, module, version):
+    print("Fetching file " + draft + " with version " + version)
+    draft_version = draft + "-" + version
+    print(draft_version)
+    os.system('curl -sO https://tools.ietf.org/id/%s.txt' %draft_version)
+    print("Extracting Module from " + draft_version)
+    os.system('xym %s.txt' %draft_version)
+    print("Moving module " + module + " to ../bin/imported-modules/")
+    os.system('mv %s* ../bin/imported-modules/' %module)
+    print("Cleaning up ...")
+    os.system('rm %s.txt' %draft_version)
+
+def fetch(module, path):
+    file = path + module + ".yang.txt"
+    print("Fetching file " + file)
+    os.system('curl -sO http://%s' %file)
+    model = module + ".yang"
+    print("Moving module " + model + " to ../bin/imported-modules/")
+    os.system("mv '%s'.txt ../bin/imported-modules/'%s'" %(model, model))
+
+os.system("mkdir -p ../bin/imported-modules")
+
+for list in list_of_ietf_models:
+    module, draft, version = list
+    print(module)
+    fetch_and_extract(draft, module, version)
+
+os.system('rm *.yang')
---
+++ src/validate-and-gen-trees.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+for i in ../bin/*\@$(date +%Y-%m-%d).yang
+do
+    name=$(echo $i | cut -f 1-3 -d '.')
+    echo "Validating YANG module $name.yang"
+    if test "${name#^example}" = "$name"; then
+        response=`pyang --lint --strict --canonical -p ../bin -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
+    else            
+        response=`pyang --ietf --strict --canonical -p ../bin -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
+    fi
+    if [ $? -ne 0 ]; then
+        printf "$name.yang failed pyang validation\n"
+        printf "$response\n\n"
+        echo
+	rm yang/*-tree.txt.tmp
+        exit 1
+    fi
+    fold -w 71 $name-tree.txt.tmp > $name-tree.txt
+    response=`yanglint -p ../src/yang $name.yang -i`
+    if [ $? -ne 0 ]; then
+       printf "$name.yang failed yanglint validation\n"
+       printf "$response\n\n"
+       echo
+       exit 1
+   fi
+   echo "$name.yang is VALID"
+done
+rm ../bin/*-tree.txt.tmp
+
+for i in ../bin/ietf-bfd-stability\@$(date +%Y-%m-%d).yang
+do
+    name=$(echo $i | cut -f 1-3 -d '.')
+    echo "Generating abridged tree diagram for $name.yang"
+    if test "${name#^example}" = "$name"; then
+       response=`pyang --lint --strict --canonical -p ../bin/submodules -p ../bin -f tree --tree-depth=7 --max-line-length=69 --tree-line-length=69 $name.yang > $name-sub-tree.txt.tmp`
+    else            
+       response=`pyang --ietf --strict --canonical -p ../bin/submodules -p ../bin -f tree --tree-depth=3 --max-line-length=69 --tree-line-length=69 $name.yang > $name-sub-tree.txt.tmp`
+    fi
+    if [ $? -ne 0 ]; then
+        printf "$name.yang failed generation of sub-tree diagram\n"
+        printf "$response\n\n"
+        echo
+	rm yang/*-sub-tree.txt.tmp
+        exit 1
+    fi
+    fold -w 69 $name-sub-tree.txt.tmp > $name-sub-tree.txt
+done
+rm ../bin/*-sub-tree.txt.tmp
